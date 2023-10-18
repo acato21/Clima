09fbdbf346eef44f04a03cb582a5b349dac4c98c
acato21 <souzaamorimdasilvai@gmail.com>
2023-10-18 20:29:49 -0300

    up

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ const key = "1719fc778e74260d6b02fc868f29703c"
 const searchCity = document.querySelector('#input')
 const city = document.querySelector('.cidade')
 const climate = document.querySelector('.temp')
-const forecast = document.querySelector('.texto-previsao')
+const situ = document.querySelector('.texto-previsao')
+const humidty = document.querySelector('.umidade')
 const search = document.querySelector('.botao-busca')
 
 search.addEventListener('click', cliquebotao)
@@ -22,7 +23,24 @@ async function dado(city){
 
     const result = await fetch(endpoint)
     .then(data => data.json())
+    .then(data => ({
 
-    console.log(result)
+        name:  data.name,
+        temp: data.main.temp,
+        weather: data.weather[0].description,
+        umidade: data.main.humidity
+    }))
+
+
+    innerHtml(result)
+
+}
+
+function innerHtml(result){
+
+    city.textContent = result.name
+    climate.textContent = `${result.temp}°`
+    situ.textContent = result.weather
+    humidty.textContent = `${result.umidade}%`
 
 }
